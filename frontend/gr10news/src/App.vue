24d57c9df927346c7976f2bef31d7ba394715a20
LISSRL <violetalis54321@gmail.com>
2022-08-23 15:36:17 -0400

    estilo de header

--- frontend/gr10news/src/App.vue
+++ frontend/gr10news/src/App.vue
@@ -1,25 +1,33 @@
 <template>
   <div id="app">
     <div class="navigation" style="background-color:rgb(3, 19, 123); box-shadow: 5px 0px 10px; ">
-    <div>
-        <img class="Liss..." title="Liseth Rojas" src='https://wpexpand.com/demo/html/kotha-pro/assets/images/me.jpg' width="60px" style="border-radius: 50%; border: 3px solid #fff;"  target="_blank"/>
-        <div class="login-register">
-         <!--  <div class="col-md-8" style="margin:15px; color: #fff; font-family:Arial, Helvetica, sans-serif;"><label class="login" id="login"></label>
-          </div>   -->
-          <router-link to="/">LOGIN</router-link>
-          <button type="button" @click="logout" class="button pull-right" style="float: right;">LOGOUT</button> 
+      <div>
+        <div style=" float: left; padding-left: 5px;" >
+          <img class="Liss..." title="Liseth Rojas" src='https://wpexpand.com/demo/html/kotha-pro/assets/images/me.jpg' width="60px" style="border-radius: 50%; border: 3px solid #fff;"  target="_blank" />
         </div>
-    </div>
-    
-      
+        <div class="col-md-8" style="margin:15px; color: #fff; font-family:Arial, Helvetica, sans-serif; float: left;"><br><label>Rojas Lopez Lis</label>
+        </div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <div class="login-register" style=" float: right;">
+        <br>
+          <router-link to="/" style="color: blanchedalmond">LOGIN</router-link>
+          <button type="button" @click="logout" class="button" style="float: right !important; color: black">LOGOUT</button> 
+        </div>
+      </div>
     </div> 
+    <hr>
     <div id="nav">
         <router-link to="/dashboard">INICIO</router-link>
         <router-link to="/cateory">CATEGORIAS</router-link>
         <router-link to="/notice">NOTICIAS</router-link>
         <router-link to="/register">USUARIOS</router-link>
     </div>
-    <hr>
+    <hr style="box-shadow: 5px 0px 10px;"/>
       
     <router-view/>
 
@@ -50,10 +58,11 @@ export default {
 }
 
 #nav {
-  padding: 30px;
+  padding: 0px;
   display: flex;
   padding-left: 5px;
   margin-left: 10px;
+  
 }
 
 #nav a {
@@ -68,8 +77,8 @@ export default {
 .navigation{
   display: flex;
   flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
+  /* justify-content: space-around;
+  align-items: right; */
 }
 </style>
 
